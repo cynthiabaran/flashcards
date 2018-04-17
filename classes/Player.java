f593831ccf9021c8e22cd97453e72bb6759709cc
Akacoll <gabriel.akacoll@gmail.com>
2018-04-16 22:06:22 -0300

    Release 0

---
+++ classes/Player.java
@@ -0,0 +1,74 @@
+package classes;
+/**
+ * @author Grupo das Bananas loucas da aldeia do mato
+ */
+
+/** Class classes.Player
+ *
+ * classes.Player description class
+ *
+ */
+
+public class Player {
+    // parameters
+    private String name;
+    private int healthPoints;
+    private static final int MAX_HEALTH = 100; // TODO: choose MAX_HEALTH value
+    private float playerPoints;
+    //private Flashcards hand; // TODO: player's card hand (valid?)
+
+    // constructors
+    Player () { // default constructor
+        // TODO: discuss which could be valid constructor for this class
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    public static int getMaxHealth() {
+        return MAX_HEALTH;
+    }
+
+    public float getPlayerPoints() {
+        return playerPoints;
+    }
+
+    public void setPlayerPoints(float playerPoints) {
+        this.playerPoints = playerPoints;
+    }
+
+    Player (String name) { // parameters constructor
+        this.name = name;
+        this.healthPoints = MAX_HEALTH;
+        this.playerPoints = 0;
+    }
+
+    public void playCard() {
+        // TODO: implement the playCard method
+    }
+
+    public void gainLife (int hp) {
+        healthPoints += hp;
+    }
+
+    public boolean loseLife (int hp) { // returns if the player loses or not
+        healthPoints -= hp;
+        if (healthPoints <= 0)
+            return true;
+        else
+            return false;
+    }
+}
---
+++ classes/Sandbox.java
@@ -0,0 +1,24 @@
+package classes; /**
+ * @author Grupo das Bananas loucas da aldeia do mato
+ */
+
+import classes.Game;
+
+/** Class classes.Sandbox
+ *
+ * classes.Sandbox game type, the player
+ * can play for how long he wants
+ * and quit whenever he wants
+ */
+
+public class Sandbox extends Game { // classes.Sandbox is a specific classes.Game type
+    // parameters
+    private float timeElapsed;
+
+    // constructors
+    Sandbox() { // default constructor
+    }
+
+    // methods
+    /* earnPoints (static from classes.TimeAttack) */
+}
