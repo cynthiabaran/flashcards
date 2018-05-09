9ff26bbfd95d851b9d33266f90735bfe2d23f402
AndradeL <l182371@g.unicamp.br>
2018-05-09 12:21:22 -0300

    Add input scanner and method makeMenu
    	makeMenu method splashs the beggining messages and
    	reads input to choose the game type then returns
    	a game of that type

--- classes/Menu.java
+++ classes/Menu.java
@@ -3,13 +3,28 @@ package classes;
  * @author Grupo das Bananas loucas da aldeia do mato
  */
 
+import Java.util.Scanner;
+
 public class Menu {
 
-    Scanner
+    Scanner scanf;
 
-    Menu(){ // TODO: define constructor(s) for this class
+    Menu(){
+        scanf = new Scanner(System.in);
+    }
+
+    public Game makeMenu() {
         showSplash();
-        showMenu();
+
+        Game type = null;
+
+        do {
+            showChoices();
+            String mode = scanf.nextString();
+            type = chooseGameType(mode);
+        } while (type == null);
+        
+        return type;
     }
 
     public void showSplash() {
@@ -21,12 +36,12 @@ public class Menu {
         System.out.println(saidaSplash);
     }
 
-    public void showMenu() {
+    public void showChoices() {
         String saidaMenu =
         "Please choose your game type:\n" +
-        "Type the number or the game type name to choose\n" +
+        "Type the number or the name to choose\n" +
             "\t1: Versus\n" +
-                "\t\tPlay against another player locally, the one to hit 10 first wins\n\n" +
+                "\t\tPlay against another player locally, the one to hit 10 first wins\n\n" + // TODO: define the number of correct answers to win, 10 was arbitrary
             "\t2: Time Attack\n" +
                 "\t\tPlay against time to see how much answers you can get right before the time ends\n\n" +
             "\t3: Sandbox\n" +
@@ -41,15 +56,23 @@ public class Menu {
 
     public Game chooseGameType(String mode) {
         Game gameType = new Game();
+        boolean validType = false;
         if (mode == "1" || mode.toUpperCase().equals("VERSUS")) {
             gameType = new Versus();
+            validType = true;
         }
         if (mode == "2" || mode.toUpperCase().equals("TIMEATTACK") || mode.toUpperCase().equals("TIME ATTACK")) {
             gameType = new TimeAttack(180); // example of max time
+            validType = true;
         }
         if (mode == "3" || mode.toUpperCase().equals("SANDBOX")) {
             gameType = new Sandbox();
+            validType = true;
         }
-        return gameType;
+        if (validType) {
+            return gameType;
+        } else {
+            return null;
+        }
     }
 }
