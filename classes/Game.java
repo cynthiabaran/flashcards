cc6a295728804f29332a0bbc08b7a96ffebde381
AndradeL <l182371@g.unicamp.br>
2018-05-13 18:48:02 +0000

    Implements populateDeck method

--- classes/Game.java
+++ classes/Game.java
@@ -1,9 +1,13 @@
 package classes;
 
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 /**
  * @author Grupo das Bananas loucas da aldeia do mato
  */
+
+import com.sun.java.util.jar.pack.Package.File;
 
 /** Class classes.Game
  *
@@ -26,6 +30,13 @@ public class Game {
     }
 
     public void populateDeck(String cardType) {
-
+        String folderPath = "./data/"+getCardType();
+        java.io.File folder = new File(folder);
+        for (File fileEntry : folder.listFiles()) {
+            FileInputStream fileIn = new FileInputStream(fileEntry);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            Card c = (Card) in.readObject();
+            deck.add(c);
+        }
     }
 }
